fff6310137a2765c74628b94a5315d0da888d618
Torsten Stremlau <torsten@stremlau.de>
2025-04-16 00:28:43 +0200

    Update CMake configuration and main game logic for improved build and execution

--- main.cpp
+++ main.cpp
@@ -1,10 +1,90 @@
+#include "platform/sdl/sound_controller_sdl.h"
+#include "platform/sdl/music_controller_sdl.h"
+#include "framebuffer_gfx.h"
+#include "shared_ptr.h"
+#include "i18n.h"
+#include "game.h"
+#include "credits_scroller.h"
+#include "exception.h"
+
 #include <SDL3/SDL.h>
 
 #include <iostream>
 #include <memory>
 
+int runGame() {
+    uint32_t frameCounter = 0;
+
+    try
+    {
+        tnd::shared_ptr<SoundController> sound(new SoundControllerSdl());
+
+     
+        I18N::loadTranslations("strings.en");
+        
+        printStr(BUILD_DATE + TinyString("\r\n") + I18N::getString(3).c_str());
+
+        tnd::shared_ptr<GfxOutput> gfx(new FramebufferGfx());
+        // Keyboard keyboard;
+    
+        GameExitCode exitCode = GAME_EXIT_QUIT;
+        {
+            tnd::shared_ptr<Animation> enemy(new Animation("enemy.ani", "enemy.tga"));
+            tnd::shared_ptr<Animation> seekerEnemy(new Animation("enemy2.ani", "enemy2.tga"));
+            tnd::shared_ptr<Animation> guffin(new Animation("guffin.ani", "guffin.tga"));
+            tnd::shared_ptr<Animation> guy(new Animation("guy.ani", "guy.tga"));
+            tnd::shared_ptr<Animation> fireBall(new Animation("fire.ani", "fire.tga"));
+            tnd::shared_ptr<Animation> jetPack(new Animation("jet.ani", "jet.tga"));
+            tnd::shared_ptr<Animation> tentacle(new Animation("tentacle.ani", "tentacle.tga"));
+            tnd::shared_ptr<Animation> projectile(new Animation("bullet.ani", "bullet.tga"));
+            tnd::shared_ptr<Animation> tentacleArm(new Animation("ten_arm.ani", "ten_arm.tga"));
+            tnd::shared_ptr<Animation> eye(new Animation("eye.ani", "eye.tga"));
+
+            tnd::shared_ptr<MusicController> music(new MusicControllerSdl());
+
+            GameAnimations animations = {guy, enemy, seekerEnemy, guffin, fireBall, jetPack, tentacle, projectile, tentacleArm, eye};
+
+            Game game(gfx, sound, music, animations, "%02x%02x", LevelNumber(1,1));
+
+           
+            exitCode = game.runGameLoop();
+            frameCounter = game.getFrameCount();
+        }
+
+        // if (exitCode == GAME_EXIT_CREDITS)
+        // {
+        //     runCredits(*gfx, "credits.txt");
+        // }
+        
+    }
+    catch(const Exception& e)
+    {
+        printStr("Exception: ");
+        printStr(e.what());
+        printStr("\r\nerrno: ");
+
+        char buf[10];
+        intToString(errno, 10, buf, 10);
+        printStr(buf);
+        printStr("\r\n");
+        return 1;
+    }
+    catch(...)
+    {
+        printStr("Unknown exception.\r\n");
+        return 1;
+    }
+
+    printStr(BUILD_DATE + TinyString("\r\n") + I18N::getString(3).c_str());
+
+    return 0;
+}
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    return runGame();
 
     if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
         std::cerr << "SDL_InitSubSystem Error: " << SDL_GetError() << std::endl;
