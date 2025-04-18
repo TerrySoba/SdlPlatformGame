0a4e1d5a5aae97493b9d24efac90626117ac84af
Torsten Stremlau <torsten@stremlau.de>
2025-04-18 15:34:22 +0200

    Update texture pixel format to RGBA8888 and adjust game window resolution

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ public:
         SDL_Rect rect = {0,0,320,200};
         SDL_LockTexture(m_renderTexture.get(), NULL, &pixels, &pitch);
 
-        m_gfx->renderToMemory(pixels, pitch, PixelFormat::PIXEL_FORMAT_RGB888);
+        m_gfx->renderToMemory(pixels, pitch, PixelFormat::PIXEL_FORMAT_RGBA8888);
 
         // unlock the texture
         SDL_UnlockTexture(m_renderTexture.get());
@@ -75,78 +75,6 @@ private:
     tnd::shared_ptr<Game> m_game;
     tnd::shared_ptr<GfxOutput> m_gfx;
 };
-
-int runGame() {
-    uint32_t frameCounter = 0;
-
-    try
-    {
-        tnd::shared_ptr<SoundController> sound(new SoundControllerSdl());
-
-     
-        I18N::loadTranslations("strings.en");
-        
-
-        tnd::shared_ptr<GfxOutput> gfx(new FramebufferGfx());
-        // Keyboard keyboard;
-    
-        GameExitCode exitCode = GAME_EXIT_QUIT;
-        {
-            tnd::shared_ptr<Animation> enemy(new Animation("enemy.ani", "enemy.tga"));
-            tnd::shared_ptr<Animation> seekerEnemy(new Animation("enemy2.ani", "enemy2.tga"));
-            tnd::shared_ptr<Animation> guffin(new Animation("guffin.ani", "guffin.tga"));
-            tnd::shared_ptr<Animation> guy(new Animation("guy.ani", "guy.tga"));
-            tnd::shared_ptr<Animation> fireBall(new Animation("fire.ani", "fire.tga"));
-            tnd::shared_ptr<Animation> jetPack(new Animation("jet.ani", "jet.tga"));
-            tnd::shared_ptr<Animation> tentacle(new Animation("tentacle.ani", "tentacle.tga"));
-            tnd::shared_ptr<Animation> projectile(new Animation("bullet.ani", "bullet.tga"));
-            tnd::shared_ptr<Animation> tentacleArm(new Animation("ten_arm.ani", "ten_arm.tga"));
-            tnd::shared_ptr<Animation> eye(new Animation("eye.ani", "eye.tga"));
-
-            tnd::shared_ptr<MusicController> music(new MusicControllerSdl());
-
-            GameAnimations animations = {guy, enemy, seekerEnemy, guffin, fireBall, jetPack, tentacle, projectile, tentacleArm, eye};
-
-            Game game(gfx, sound, music, animations, "%02x%02x", LevelNumber(1,1));
-
-            // game.drawFrame();
-            // gfx->saveAsTgaImage("test.tga");
-           
-            // exitCode = game.runGameLoop();
-            // frameCounter = game.getFrameCount();
-        }
-
-        // if (exitCode == GAME_EXIT_CREDITS)
-        // {
-        //     runCredits(*gfx, "credits.txt");
-        // }
-        
-    }
-    catch(const Exception& e)
-    {
-        printStr("Exception: ");
-        printStr(e.what());
-        printStr("\r\nerrno: ");
-
-        char buf[10];
-        intToString(errno, 10, buf, 10);
-        printStr(buf);
-        printStr("\r\n");
-        return 1;
-    }
-    catch(...)
-    {
-        printStr("Unknown exception.\r\n");
-        return 1;
-    }
-
-    printStr(BUILD_DATE + TinyString("\r\n") + I18N::getString(3).c_str());
-    printf("Frame count: %d\n", frameCounter);
-    fflush(stdout);
-
-    return 0;
-}
-
 
 class ScreenSizeHelper {
 public:
@@ -205,8 +133,8 @@ private:
 
 
 int main() {
-    const uint32_t gameWindowResolutionWidth = 1280;
-    const uint32_t gameWindowResolutionHeight = 720;
+    const uint32_t gameWindowResolutionWidth = 2304;
+    const uint32_t gameWindowResolutionHeight = 1728;
     const float dosGameAspectRatio = 4.0 / 3.0;
 
     try {
@@ -215,6 +143,16 @@ int main() {
         if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
             std::cerr << "SDL_InitSubSystem Error: " << SDL_GetError() << std::endl;
             return 1;
+        }
+
+        const char *videoDriver = SDL_GetCurrentVideoDriver();
+        if (videoDriver)
+        {
+            std::cout << "Current video driver: " << videoDriver << std::endl;
+        }
+        else
+        {
+            std::cerr << "Failed to get video driver." << std::endl;
         }
 
         std::shared_ptr<SDL_Window> win(SDL_CreateWindow("Hello SDL", gameWindowResolutionWidth, gameWindowResolutionHeight, 0), SDL_DestroyWindow);
@@ -247,7 +185,7 @@ int main() {
 
         // std::shared_ptr<SDL_Texture> tex(SDL_CreateTextureFromSurface(ren.get(), bmp.get()), SDL_DestroyTexture);
         
-        std::shared_ptr<SDL_Texture> tex(SDL_CreateTexture(ren.get(), SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, 320, 200), SDL_DestroyTexture);
+        std::shared_ptr<SDL_Texture> tex(SDL_CreateTexture(ren.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 320, 200), SDL_DestroyTexture);
         if (!tex) {
             std::cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
             SDL_Quit();
@@ -332,8 +270,8 @@ int main() {
                 
                 SDL_RenderClear(ren.get());
                 SDL_FRect dst = {
-                    screenSizeHelper.getRenderOffsetX(), screenSizeHelper.getRenderOffsetY(),
-                    screenSizeHelper.getRenderWidth(), screenSizeHelper.getRenderHeight()};
+                    (float)screenSizeHelper.getRenderOffsetX(), (float)screenSizeHelper.getRenderOffsetY(),
+                    (float)screenSizeHelper.getRenderWidth(), (float)screenSizeHelper.getRenderHeight()};
                 SDL_RenderTexture(ren.get(), tex.get(), nullptr, &dst);
                 SDL_RenderPresent(ren.get());
 
